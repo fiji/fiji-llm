a5a06e467d862d1f094ad540bd4ea3b71ec362f9
Mark Hiner <hinerm@gmail.com>
2025-10-31 14:46:35 -0500

    Remove the LLMContextService
    We'll handle these things via Tools as needed

--- src/main/java/sc/fiji/llm/ui/SwingChatbotService.java
+++ src/main/java/sc/fiji/llm/ui/SwingChatbotService.java
@@ -10,16 +10,12 @@ import org.scijava.service.AbstractService;
 import org.scijava.service.Service;
 
 import sc.fiji.llm.assistant.FijiAssistant;
-import sc.fiji.llm.service.LLMContextService;
 
 /**
  * Swing implementation of ChatbotService that launches a SimpleChatWindow.
  */
 @Plugin(type = Service.class)
 public class SwingChatbotService extends AbstractService implements ChatbotService {
-
-    @Parameter
-    public LLMContextService llmContextService;
 
     @Parameter
     public CommandService commandService;
@@ -30,8 +26,7 @@ public class SwingChatbotService extends AbstractService implements ChatbotServi
     @Override
     public void launchChat(FijiAssistant assistant, String title) {
         SwingUtilities.invokeLater(() -> {
-            SimpleChatWindow chatWindow = new SimpleChatWindow(assistant, llmContextService, 
-                                                               commandService, prefService, title);
+            SimpleChatWindow chatWindow = new SimpleChatWindow(assistant, commandService, prefService, title);
             chatWindow.show();
         });
     }
