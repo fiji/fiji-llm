f39993c221d4af06d2a55ba8cd2829b8ad26da03
Mark Hiner <hinerm@gmail.com>
2025-11-21 12:08:34 -0600

    wip: app context

---
+++ src/main/java/sc/fiji/llm/context/DefaultAppContextService.java
@@ -0,0 +1,47 @@
+/*-
+ * #%L
+ * A Fiji plugin for integrating large language models.
+ * %%
+ * Copyright (C) 2025 ImageJ Developers
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+package sc.fiji.llm.context;
+
+import org.scijava.plugin.AbstractSingletonService;
+import org.scijava.plugin.Plugin;
+import org.scijava.service.Service;
+
+import com.google.gson.JsonArray;
+
+@Plugin(type = Service.class)
+public class DefaultAppContextService extends AbstractSingletonService<AppContextSupplier> implements AppContextService {
+
+    @Override
+    public Class<AppContextSupplier> getPluginType() {
+		return AppContextSupplier.class;
+    }
+
+    @Override
+    public JsonArray getCurrentEnvironment() {
+		JsonArray appEnv = new JsonArray();
+		for (AppContextSupplier supplier : getInstances()) {
+			appEnv.add(supplier.appConext());
+		}
+		return appEnv;
+    }
+	
+}
--- src/main/java/sc/fiji/llm/ui/FijiAssistantChat.java
+++ src/main/java/sc/fiji/llm/ui/FijiAssistantChat.java
@@ -85,6 +85,7 @@ import sc.fiji.llm.chat.Conversation;
 import sc.fiji.llm.chat.ConversationService;
 import sc.fiji.llm.commands.Fiji_Chat;
 import sc.fiji.llm.commands.Manage_Keys;
+import sc.fiji.llm.context.AppContextService;
 import sc.fiji.llm.context.ContextItem;
 import sc.fiji.llm.context.ContextItemService;
 import sc.fiji.llm.context.ContextItemSupplier;
@@ -127,6 +128,9 @@ public class FijiAssistantChat {
 
 	@Parameter
 	private ContextItemService contextItemService;
+
+	@Parameter
+	private AppContextService appContextService;
 
 	@Parameter
 	private ThreadService threadService;
@@ -827,9 +831,21 @@ public class FijiAssistantChat {
 				final UserMessage.Builder msgBuilder = UserMessage.builder()
 						.addContent(new TextContent(userText));
 
-				// Attach context items as message attributes
+				Map<String, Object> attributes = new HashMap<>();
+
+				// Attach user context from user
 				if (!mergedContextItems.isEmpty()) {
-					msgBuilder.attributes(Map.of("context:from_user", userContextArray.toString()));
+					attributes.put("context:from_user", userContextArray);
+				}
+
+				// Attach environment context
+				JsonArray appContext = appContextService.getCurrentEnvironment();
+				if (!appContext.isEmpty()) {
+					attributes.put("context:from_app", appContext);
+				}
+
+				if (!attributes.isEmpty()) {
+					msgBuilder.attributes(attributes);
 				}
 
 				final UserMessage userMsg = msgBuilder.build();
